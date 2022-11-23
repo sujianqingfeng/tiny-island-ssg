ae494aa8e1b5e7ee7969d9ded5768ee4967e24b9
hens <121116111@qq.com>
2022-11-24 00:58:26 +0800

    feat: default theme

---
+++ src/runtime/client-entry.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './app'
+
+function renderInBrowser() {
+  const el = document.getElementById('root')
+  if (!el) {
+    throw new Error('No element with id "root" found')
+  }
+
+  createRoot(el).render(<App />)
+}
+
+renderInBrowser()
