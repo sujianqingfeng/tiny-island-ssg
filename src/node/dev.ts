2f1274ade4aaf764a34edba112a89ff08eba171a
hens <121116111@qq.com>
2022-11-25 23:23:38 +0800

    wip: index-html plugin update

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,12 +1,14 @@
 import { createServer as createViteDevServer } from 'vite';
 import { pluginIndexHtml } from './plugin-island/index-html';
+import pluginReact from '@vitejs/plugin-react'
 
 export async function createDevServer(root = process.cwd()){
 
   return createViteDevServer({
     root,
     plugins:[
-      pluginIndexHtml()
+      pluginIndexHtml(),
+      pluginReact()
     ]
   })
 }
--- src/node/plugin-island/index-html.ts
+++ src/node/plugin-island/index-html.ts
@@ -1,17 +1,32 @@
 import { readFile } from 'fs/promises';
 import type { Plugin } from 'vite';
-import { DEFAULT_HTML_PATH } from '../constants';
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from '../constants';
 
 export function pluginIndexHtml():Plugin {
   return {
     name:'island:index-html',
     apply:'serve',
-    configureServer(serve){
+    transformIndexHtml(html){
+      return {
+        html,
+        tags:[
+          {
+            tag:'script',
+            attrs:{
+              type:'module',
+              src:`/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo:'body'
+          }
+        ]
+      }
+    },
+    configureServer(server){
       return ()=>{
-        serve.middlewares.use(async (req,res,next)=>{
-
-          let html = await readFile(DEFAULT_HTML_PATH)
+        server.middlewares.use(async (req,res,next)=>{
+          let html = await readFile(DEFAULT_HTML_PATH,'utf-8')
           try {
+            html = await server.transformIndexHtml(req.url,html,req.originalUrl)
             res.statusCode = 200
             res.setHeader('Content-Type','text/html')
             res.end(html)
