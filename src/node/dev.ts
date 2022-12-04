310dbe485f76dd444958380444ed5cdd3672d4cd
hens <121116111@qq.com>
2022-12-04 21:50:32 +0800

    feat: add playwright

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,10 +1,16 @@
 import { createServer as createViteDevServer } from 'vite'
 import { pluginIndexHtml } from './plugin-island/index-html'
 import pluginReact from '@vitejs/plugin-react'
+import { PACKAGE_ROOT } from './constants'
 
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
-    plugins: [pluginIndexHtml(), pluginReact()]
+    plugins: [pluginIndexHtml(), pluginReact()],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
+    }
   })
 }
